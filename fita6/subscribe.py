66c7f4f39f9fc1189b61e5ade0fb54829c63b2e0
marcdelarratea <1396920@campus.euss.org>
2020-12-14 16:40:19 +0100

    afegint totes les dades a la base de dades local

--- fita6/subscribe.py
+++ fita6/subscribe.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 import paho.mqtt.client as mqtt
 import requests
+import sqlite3
+from sqlite3 import Error
+from datetime import date
+from datetime import datetime
+
+def cadenaTime():
+    today = date.today()
+    now = datetime.now()
+    d1 = today.strftime("%d/%m/%Y")
+    current_time = now.strftime("%H:%M:%S")
+    valueTemps=str(d1+' '+current_time)
+    print (valueTemps)
+    return(valueTemps)
+
+
 #from pymongo import MongoClient
 ipadress = '192.168.11.218'
 #cliente = MongoClient("mongodb+srv://skril:Pereieva1@cluster0-eufis.mongodb.net/test?retryWrites=true&w=majority")
@@ -11,6 +26,36 @@ ipadress = '192.168.11.218'
 
 #Lluminositat.insert_one({"Lluminositat":"3"})
 #Humitat.insert_one({"Humitat":"5"})
+
+
+    
+def sql_insert(con, entities):
+
+    cursorObj = con.cursor()
+    
+    cursorObj.execute('INSERT INTO fita6mqtt(Id,Sensor,Valor,Temps) VALUES(?, ?, ?, ?)', entities)
+    
+    con.commit()
+    
+def sql_connection():
+    try:
+        con = sqlite3.connect('temperatures.db')
+        return con
+    except Error:
+        print(Error)
+        
+def sql_table(con):
+    cursorObj = con.cursor()
+    cursorObj.execute("CREATE TABLE fita6mqtt(Id integer,Sensor text, Valor text, Temps text)")
+    con.commit()
+#per crear la taula executem les següents dues linies de comandes: 
+con = sql_connection()
+#sql_table(con)   
+    
+    
+    
+    
+    
     
 def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
     print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
@@ -29,6 +74,12 @@ def on_message(client, userdata, msg):  # The callback for when a PUBLISH messag
         cadena = 'http://iotlab.euss.es/cloud/guardar_dades_adaptat.php?id_sensor=81&valor={0}&temps='.format(str(msg.payload))
         print(cadena)
         r =requests.get(cadena)
+        str(msg.payload)
+        print(type(msg.payload))
+        temps = str(cadenaTime())
+        entities = (81, 'Temperatura', str(msg.payload),temps)
+        sql_insert(con, entities)
+        
         print(r)
         #Temperatura.insert_one(temp)
         
@@ -38,6 +89,9 @@ def on_message(client, userdata, msg):  # The callback for when a PUBLISH messag
         print(cadena1)
         r1 =requests.get(cadena1)
         print(r1)
+        temps = str(cadenaTime())
+        entities = (82, 'Humitat', str(msg.payload),temps)
+        sql_insert(con, entities)
        # Humitat.insert_one(hum)
         
     if(msg.topic == "Pressure"):
@@ -46,6 +100,9 @@ def on_message(client, userdata, msg):  # The callback for when a PUBLISH messag
         print(cadena2)
         r2 =requests.get(cadena2)
         print(r2)
+        temps = str(cadenaTime())
+        entities = (83, 'Pressure', str(msg.payload),temps)
+        sql_insert(con, entities)
        # Lluminositat.insert_one(llum)    
 
 
